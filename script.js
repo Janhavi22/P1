a5e74332eef77538466cffaea142770a60a6aefe
janhavi22 <janhavijchitade@gmail.com>
2023-08-21 20:23:34 +0530

    file

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+var tl = gsap.timeline()
+
+function time(){
+    var a=0
+    setInterval(function(){
+        a = a + Math.floor(Math.random()*20)
+        if(a<100){
+            document.querySelector("#loader h1").innerHTML = a+"%"
+        }else{
+            a = 100
+            document.querySelector("#loader h1").innerHTML = a+"%"
+        }
+        
+    },150)
+}
+tl.to("#loader h1",{
+    delay:0.5,
+    duration:1,
+    onStart:time()
+})
+tl.to("#loader",{
+    top:"-100vh",
+    delay:0.4,
+    duration:1.5
+})
+
 gsap.to("#page1 h1",{
     transform:"translateX(-100%)",
     fontWeight:"100",
@@ -11,3 +37,28 @@ gsap.to("#page1 h1",{
         pin:true
     }
 })
+//  const scroll = new LocomotiveScroll({
+//      el: document.querySelector('#main'),
+//      smooth: true
+// });
+// var tl = gsap.timeline()
+// tl.from("#page2",{
+//     width:300,
+
+//     scrollTrigger:{
+//         trigger:"#page2",
+//         scroller:"body",
+//         start:"50% ",
+//     }
+// })
+// tl.to("#page2",{
+//     y:"30vh",
+//     duration:1,
+//     delay:1
+// })
+// tl.to("#page2",{
+//     y:"00vh",
+//     rotate:360,
+//     scale:1,
+//     duration:0.8
+// })
